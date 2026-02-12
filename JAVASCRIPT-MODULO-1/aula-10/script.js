b4d1254ba09191fd3214b08f4842aab228943638
Fernando Bezerra (Nokia) <fernando.bezerra@nokia.com>
2026-02-12 11:30:25 -0500

    Class 10 - Concluded

---
+++ JAVASCRIPT-MODULO-1/aula-10/script.js
@@ -0,0 +1,13 @@
+let myName = "Fernando"
+
+console.log(myName)
+console.error(myName)
+console.warn(myName)
+
+// window.alert(myName)
+// myName = window.prompt("Enter with your name: ")
+// let confirmation = window.confirm("Confirm this action?")
+
+
+
+
