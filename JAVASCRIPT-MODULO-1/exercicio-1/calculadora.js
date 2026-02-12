11c9646e29a2ad0c70e2d17ab37a32461f5b72a3
Fernando Bezerra (Nokia) <fernando.bezerra@nokia.com>
2026-02-12 12:42:11 -0500

    Exercise Calculadora of the JavaScript Module 1 - Concluded

---
+++ JAVASCRIPT-MODULO-1/exercicio-1/calculadora.js
@@ -0,0 +1,15 @@
+let num1 = parseInt(prompt("Type the first number"))
+let num2 = parseInt(prompt("Type the second number"))
+
+let addition = num1 + num2
+let subtraction = num1 - num2
+let multiplication = num1 * num2
+let division = parseInt(num1 / num2)
+
+alert(
+    "Result:\n" +
+    "\nSum: " + addition +
+    "\nMinus: " + subtraction +
+    "\nMultiply: " + multiplication +
+    "\nDivide: " + division 
+)
