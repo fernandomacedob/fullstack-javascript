ab5b6cb497ee9cfc19cf7a1f403d13129aa889aa
Fernando Bezerra (Nokia) <fernando.bezerra@nokia.com>
2026-02-12 13:47:55 -0500

    Class 16 - Conditionals IF and ELSE - Concluded

---
+++ JAVASCRIPT-MODULO-1/aula-16/script.js
@@ -0,0 +1,9 @@
+const age = prompt("Type your age:");
+
+if (age >= 18) {
+	console.log("You're an adult");
+} else if (age >= 12) {
+	console.log("You're a minor");
+} else if (age > 4) {
+	console.log("You're a child");
+}
