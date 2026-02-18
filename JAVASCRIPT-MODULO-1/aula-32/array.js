9d18466f1fd871940d84011e534487cff956cbd0
Fernando Bezerra (Nokia) <fernando.bezerra@nokia.com>
2026-02-18 14:08:33 -0500

    Class 32 - Arrays - Concluded

---
+++ JAVASCRIPT-MODULO-1/aula-32/array.js
@@ -0,0 +1,14 @@
+const shoppingList = ["Sugar", false]
+console.log(shoppingList)
+
+shoppingList[0] = "Rice"
+shoppingList[1] = "Beans"
+shoppingList[2] = 7
+shoppingList[5] = "Potatoes"
+
+
+console.log(shoppingList)
+
+console.log(shoppingList[4])
+
+
