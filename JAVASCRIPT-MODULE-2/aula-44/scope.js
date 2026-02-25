9a87191ee0feadb958196fb7118e65a3fa9611c9
Fernando Bezerra <fernandomb@gmail.com>
2026-02-25 13:12:23 -0500

    Class 44 - Scope - JavaScript Module 2 - Concluded

---
+++ JAVASCRIPT-MODULE-2/aula-44/scope.js
@@ -0,0 +1,50 @@
+// let pokemon = "Charmander"
+
+// function evolution() {
+//     pokemon = "Charmeleon"
+// }
+
+// console.log(pokemon)
+// evolution()
+// console.log(pokemon)
+
+// function createAnimal() {
+//     let animal = "cat"
+// }
+
+// createAnimal()
+// console.log(animal)
+
+// function validateGrade(grade) {
+//     if (grade >= 7) {
+//         var approved = true
+//         let situation = "Approved"
+//     } else {
+//         var approved = false
+//         let situation = "Reproved"
+//     }
+
+//     console.log(grade)
+//     console.log(approved)
+//     console.log(situation)
+// }
+
+// validateGrade(8)
+// validateGrade(6)
+
+// function hi() {
+//     var text = "John"
+    
+// }
+
+// console.log(text)
+
+console.log(firstName)
+// console.log(surname)
+
+var firstName = "Isaac"
+let surname = "Pontes"
+
+console.log(firstName)
+// console.log(surname)
+
